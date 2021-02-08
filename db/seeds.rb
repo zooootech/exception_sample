ba9cfc9b10b0368eaf2109cf48d921ceccbb86e5
zooootech <yzmisng@gmail.com>
2021-02-08 17:17:52 +0900

    activerecord-importを利用した初期データの作成について

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 users = []
+# usersに10000件新規ユーザーの情報を格納する
 10000.times do |i|
   users << User.new(name: "dummy-#{i+1}", ticket_count: 0)
 end
+# importメソッドの引数に配列を渡して、まとめてレコードを作成する
 User.import users
 # ticket_countをint型で許容できる最大の値にする
 User.find(500).update(ticket_count: 2147483647)
